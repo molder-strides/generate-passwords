4d30243369d6bc8519b44b88aa0b0244f5ef860d
Ingrid Jansen <153081820+molder-strides@users.noreply.github.com>
2023-12-06 18:12:55 +0700

    Create PasswordStrengthChecker.java

---
+++ PasswordStrengthChecker.java
@@ -0,0 +1,8 @@
+public class PasswordStrengthChecker {
+    public static String checkPasswordStrength(String password) {
+        // Implement password strength checking logic here
+        // Example: Check for uppercase letters, lowercase letters, digits, and special characters
+        // Return a message indicating the strength level
+        return "Strong"; // Placeholder message
+    }
+}
